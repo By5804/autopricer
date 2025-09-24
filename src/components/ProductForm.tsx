c17f62d68bf9f801d8c4089a601bd4557beb45eb
[dyad] <Budianto5804@gmail.com>
2025-09-25 02:36:47 +0700

    [dyad] Memperbaiki error 'FormDescription is not defined' - wrote 1 file(s)

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -5,7 +5,7 @@ import * as z from 'zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogDescription } from '@/components/ui/dialog';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Textarea } from '@/components/ui/textarea';
 import { Upload } from 'lucide-react';
 import { showError } from '@/utils/toast';
